437042aecbd5180637fb36add8e970240bd8ac29
Randall Lough <randallmlough@gmail.com>
2020-04-02 14:41:12 -0700

    graphql server authentication

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,4 +1,8 @@
-export const typeDefs = `
+const mongoose = require('mongoose');
+const { makeExecutableSchema } = require('graphql-tools');
+const User = mongoose.model('User');
+
+const typeDefs = `
   type User {
     _id: ID!
     email: String!
@@ -15,3 +19,31 @@ export const typeDefs = `
     token: String
   }
 `;
+
+const resolvers = {
+  Query: {
+    me(_, __, context) {
+      return context.user;
+    },
+  },
+  Mutation: {
+    login(_, { email, password }) {
+      // login method used in MERN project
+      return User.login(email, password);
+    },
+    signUp(_, { email, password }) {
+      return User.signUp(email, password);
+    },
+  },
+  User: {},
+};
+
+module.exports = {
+  schema: makeExecutableSchema({
+    typeDefs,
+    resolvers,
+    logger: { log: (e) => console.log('\x1b[31m%s\x1b[0m', e.message) },
+  }),
+  typeDefs,
+  resolvers,
+};
