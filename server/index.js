8cbeea58ed79290ac277ca59a995eb2d5d59f260
Randall Lough <randallmlough@gmail.com>
2020-04-02 12:05:25 -0700

    server client lint rules

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
+const express = require('express');
+const { port } = require('./config');
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
